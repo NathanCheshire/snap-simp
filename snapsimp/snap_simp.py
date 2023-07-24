59701495e64cb42205da0d0e99e7b06225045e5c
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2023-07-23 20:07:03 -0500

    add account parsing parse device information method

--- snapsimp/snap_simp.py
+++ snapsimp/snap_simp.py
@@ -1,7 +1,7 @@
 import argparse
 from snaps.snapchat_snap_conversation import SnapchatSnapConversation
 from soup.snap_history_parsing import extract_snap_history
-from soup.account_parsing import parse_basic_user_info
+from soup.account_parsing import parse_basic_user_info, parse_device_information
 from snaps.filtering import get_snaps_by_top_receiver, get_snaps_by_top_sender, filter_snaps_by_type
 from snaps.statistics import get_days_top_sender_sent, get_days_top_sender_did_not_send, get_days_top_receiver_did_not_receive, get_days_top_receiver_received
 
@@ -12,6 +12,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     basic_user_info = parse_basic_user_info(args.account_file)
+    device_information = parse_device_information(args.account_file)
+    print(device_information)
+
     received, sent = extract_snap_history(args.snap_history_file, basic_user_info.username) 
 
     top_sender_to_me_snaps = get_snaps_by_top_sender(received)
--- snapsimp/soup/account_parsing.py
+++ snapsimp/soup/account_parsing.py
@@ -51,7 +51,41 @@ def parse_device_information(filename: str) -> DeviceInformation:
     :param filename: the path to the html file containing the device information data
     :return: a DeviceInformation object
     """
-    pass
+    with open(filename, 'r') as f:
+        soup = BeautifulSoup(f.read(), 'html.parser')
+
+    headers = soup.find_all(HtmlHeaders.H3.value)
+
+    if len(headers) != len(HTML_HEADERS):
+        raise ValueError(f"Unexpected number of {HtmlHeaders.H3.value} headers. Expected {len(HTML_HEADERS)}, found {len(headers)}")
+
+    for i in range(len(HTML_HEADERS)):
+        if headers[i].text != HTML_HEADERS[i]:
+            raise ValueError(f"Unexpected {HtmlHeaders.H3.value} header at position {i}. Expected '{HTML_HEADERS[i]}', found '{headers[i].text}'")
+
+    tables = soup.find_all(TableElements.TABLE.value)
+    device_information_table = tables[1]
+
+    rows = device_information_table.find_all(TableElements.TABLE_ROW.value)
+    make_row = rows[0]
+    model_row = rows[1]
+    model_name_row = rows[2]
+    user_agent_row = rows[3]
+    language_row = rows[4]
+    os_type_row = rows[5]
+    os_version_row = rows[6]
+    connection_type_row = rows[7]
+
+    make = make_row.find_all(TableElements.TABLE_HEADER.value)[1].text
+    model_id = model_row.find_all(TableElements.TABLE_HEADER.value)[1].text
+    model_name = model_name_row.find_all(TableElements.TABLE_HEADER.value)[1].text
+    user_agent = user_agent_row.find_all(TableElements.TABLE_HEADER.value)[1].text
+    language = language_row.find_all(TableElements.TABLE_HEADER.value)[1].text
+    os_type = os_type_row.find_all(TableElements.TABLE_HEADER.value)[1].text
+    os_version = os_version_row.find_all(TableElements.TABLE_HEADER.value)[1].text
+    connection_type = [type.strip() for type in connection_type_row.find_all(TableElements.TABLE_HEADER.value)[1].text.split(',')]
+
+    return DeviceInformation(make, model_id, model_name, user_agent, language, os_type, os_version, connection_type)
 
 def parse_device_history(filename: str) -> List[DeviceHistory]:
     """
