981b045914b21eebdbbd9d3a488f22c48e3d70ca
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2023-07-23 21:54:08 -0500

    finish device history parsing

--- snapsimp/soup/account_parsing.py
+++ snapsimp/soup/account_parsing.py
@@ -13,6 +13,21 @@ from bs4 import BeautifulSoup
 
 HTML_HEADERS = ["Basic Information", "Device Information", "Device History", "Login History"]
 
+class DeviceHistoryLabel(Enum):
+    MAKE = "Make:"
+    MODEL = "Model:"
+    START_TIME = "Start Time:"
+    DEVICE_TYPE = "Device Type:"
+
+class LoginHistoryLabel(Enum):
+    IP = "IP:"
+    COUNTRY = "Country:"
+    CREATED = "Created:"
+    STATUS = "Status:"
+    DEVICE = "Device:"
+
+deivce_history_labels = [DeviceHistoryLabel.MAKE.value, DeviceHistoryLabel.MODEL.value, DeviceHistoryLabel.START_TIME.value, DeviceHistoryLabel.DEVICE_TYPE.value]
+
 
 def parse_basic_user_info(filename: str) -> BasicUserInfo:
     """
@@ -90,13 +105,7 @@ def parse_device_information(filename: str) -> DeviceInformation:
 
     return DeviceInformation(make, model_id, model_name, user_agent, language, os_type, os_version, connection_type)
 
-class DeviceLabels(Enum):
-    MAKE = "Make:"
-    MODEL = "Model:"
-    START_TIME = "Start Time:"
-    DEVICE_TYPE = "Device Type:"   
-
-def __parse_device_history_html(row) -> DeviceHistory:
+def __parse_device_history_row(row) -> DeviceHistory:
     """
     Extracts the device history from a BeautifulSoup row object.
 
@@ -104,20 +113,19 @@ def __parse_device_history_html(row) -> DeviceHistory:
     :return: a DeviceHistory object
     """
     row_str = re.sub("<.*?>", "", str(row))
-    labels = [DeviceLabels.MAKE.value, DeviceLabels.MODEL.value, DeviceLabels.START_TIME.value, DeviceLabels.DEVICE_TYPE.value]
     device_info = {}
 
-    for i in range(len(labels)-1):
-        start = row_str.find(labels[i]) + len(labels[i])
-        end = row_str.find(labels[i+1])
-        device_info[labels[i]] = row_str[start:end].strip()
+    for i in range(len(deivce_history_labels)-1):
+        start = row_str.find(deivce_history_labels[i]) + len(deivce_history_labels[i])
+        end = row_str.find(deivce_history_labels[i+1])
+        device_info[deivce_history_labels[i]] = row_str[start:end].strip()
 
-    device_info[labels[-1]] = row_str[row_str.find(labels[-1]) + len(labels[-1]):].strip()
+    device_info[deivce_history_labels[-1]] = row_str[row_str.find(deivce_history_labels[-1]) + len(deivce_history_labels[-1]):].strip()
 
-    return DeviceHistory(make=device_info[DeviceLabels.MAKE.value],
-                         model=device_info[DeviceLabels.MODEL.value],
-                         start_time=device_info[DeviceLabels.START_TIME.value],
-                         device_type=device_info[DeviceLabels.DEVICE_TYPE.value])
+    return DeviceHistory(make=device_info[DeviceHistoryLabel.MAKE.value],
+                         model=device_info[DeviceHistoryLabel.MODEL.value],
+                         start_time=device_info[DeviceHistoryLabel.START_TIME.value],
+                         device_type=device_info[DeviceHistoryLabel.DEVICE_TYPE.value])
 
 def parse_device_history(filename: str) -> List[DeviceHistory]:
     """
@@ -142,8 +150,33 @@ def parse_device_history(filename: str) -> List[DeviceHistory]:
     device_history_table = tables[2]
     rows = device_history_table.find_all(TableElements.TABLE_ROW.value)
 
-    device_histories = [__parse_device_history_html(row) for row in rows]
+    device_histories = [__parse_device_history_row(row) for row in rows]
     return device_histories
+
+def __parse_login_history_row(row) -> LoginHistory:
+    """
+    Extracts the login history from a BeautifulSoup row object.
+
+    :param row: BeautifulSoup object representing a table row containing the login history data
+    :return: a LoginHistory object
+    """
+    row_str = re.sub("<.*?>", "", str(row))
+    labels = [label.value for label in LoginHistoryLabel]
+    login_info = {}
+
+    for i in range(len(labels)-1):
+        start = row_str.find(labels[i]) + len(labels[i])
+        end = row_str.find(labels[i+1])
+        login_info[labels[i]] = row_str[start:end].strip()
+
+    login_info[labels[-1]] = row_str[row_str.find(labels[-1]) + len(labels[-1]):].strip()
+
+    return LoginHistory(ip=login_info[LoginHistoryLabel.IP.value],
+                        country=login_info[LoginHistoryLabel.COUNTRY.value],
+                        created=login_info[LoginHistoryLabel.CREATED.value],
+                        status=login_info[LoginHistoryLabel.STATUS.value],
+                        device=login_info[LoginHistoryLabel.DEVICE.value])
+
 
 def parse_login_history(filename: str) -> List[LoginHistory]:
     """
@@ -152,4 +185,21 @@ def parse_login_history(filename: str) -> List[LoginHistory]:
     :param filename: the path to the html file containing the login history data
     :return: a LoginHistory object
     """
-    pass
+    with open(filename, 'r') as f:
+        soup = BeautifulSoup(f.read(), 'html.parser')
+
+    headers = soup.find_all(HtmlHeaders.H3.value)
+
+    if len(headers) != len(HTML_HEADERS):
+        raise ValueError(f"Unexpected number of {HtmlHeaders.H3.value} headers. Expected {len(HTML_HEADERS)}, found {len(headers)}")
+
+    for i in range(len(HTML_HEADERS)):
+        if headers[i].text != HTML_HEADERS[i]:
+            raise ValueError(f"Unexpected {HtmlHeaders.H3.value} header at position {i}. Expected '{HTML_HEADERS[i]}', found '{headers[i].text}'")
+
+    tables = soup.find_all(TableElements.TABLE.value)
+    device_history_table = tables[3]
+    rows = device_history_table.find_all(TableElements.TABLE_ROW.value)
+
+    login_histories = [__parse_login_history_row(row) for row in rows]
+    return login_histories
