40036aaf81b99e9e428db192489ffabfeab8f35a
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2023-07-31 22:13:18 -0500

    comments

--- snapsimp/snaps/snap_history_table_column_indicie.py
+++ snapsimp/snaps/snap_history_table_column_indicie.py
@@ -2,6 +2,10 @@ from common.snap_simp_enum import SnapSimpEnum
 
 
 class SnapHistoryTableColumnIndicie(SnapSimpEnum):
+    """
+    Indicies for extracting information from a particular snap history table row containing multiple cells.
+    """
+
     SENDER = 0
     TYPE = 1
     TIME_STAMP = 2
--- snapsimp/snaps/snap_type.py
+++ snapsimp/snaps/snap_type.py
@@ -2,5 +2,8 @@ from enum import Enum
 
 
 class SnapType(Enum):
+    """
+    The type of a snap that of image of video.
+    """
     IMAGE = "IMAGE"
     VIDEO = "VIDEO"
