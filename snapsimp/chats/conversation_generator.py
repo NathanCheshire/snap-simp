8e44fd719713ebdfe8ebb74e3c51f6e0758b72c4
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2023-08-04 12:22:47 -0500

    finishing conversation generation functions

--- snapsimp/chats/conversation_generator.py
+++ snapsimp/chats/conversation_generator.py
@@ -1,29 +1,30 @@
+import os
 from typing import List
 from chats.snapchat_chat_conversation import SnapchatChatConversation
 from chats.chat import Chat
 from snaps.filtering import (
     get_by_receiving_user,
     get_by_sending_user,
-    get_by_top_receiver,
-    get_by_top_sender,
     get_top_receiver_username,
     get_top_sender_username,
 )
+from snaps.statistics import get_count
 
 
 def generate_conversation_with(
-    their_name: str, chats: List[Chat]
+    their_name: str, sent_chats: List[Chat], received_chats: List[Chat]
 ) -> SnapchatChatConversation:
     """
     Generates a snapchat chat conversation between the two snapchatters.
 
     :param their_name: the other snapchatter's username
-    :param chats: the list of all sent and received chats
+    :param sent_chats: the list of chats you've sent
+    :param received_chats: the list of chats you've received
     :return: a snapchat chat conversation object representing the conversation between you and the other person
     """
 
-    chats_to_them = get_by_receiving_user(chats, their_name)
-    chats_from_them = get_by_sending_user(chats, their_name)
+    chats_to_them = get_by_receiving_user(sent_chats, their_name)
+    chats_from_them = get_by_sending_user(received_chats, their_name)
     all_chats = chats_to_them + chats_from_them
 
     return SnapchatChatConversation(all_chats)
@@ -43,7 +44,7 @@ def generate_conversation_with_top_sender(
 
     top_sender = get_top_sender_username(received_chats)
     chats_from_them = get_by_sending_user(received_chats, top_sender)
-    chats_to_them = get_by_receiving_user(received_chats)
+    chats_to_them = get_by_receiving_user(sent_chats, top_sender)
     all_chats = chats_to_them + chats_from_them
 
     return SnapchatChatConversation(all_chats)
@@ -70,25 +71,63 @@ def generate_conversation_with_top_receiver(
 
 
 def generate_conversations(
-    chats: List[Chat],
+    my_name: str, sent_chats: List[Chat], received_chats: List[Chat]
 ) -> List[SnapchatChatConversation]:
     """
     Generates all snapchat chat conversations between all unique sender and receiver pairs.
 
-    :param chats: the list of all chat objects
+    :param my_name: your snapchat username
+    :param sent_chats: the list of chats you've received
+    :param received_chats: the list of chats you've recieved
     :return: all snapchat chat conversation objects for all unique sender and receiver pairs
     """
-    pass
+
+    sender_counts, receiver_counts = get_count(sent_chats + received_chats)
+    all_usernames = list(sender_counts.keys()) + list(receiver_counts.keys())
+    unique_usernames = list(set(all_usernames))
+
+    ret = []
+
+    for unique_username in unique_usernames:
+        if unique_username == my_name:
+            continue
+
+        conversation = generate_conversation_with(
+            unique_username, sent_chats, received_chats
+        )
+        ret.append(conversation)
+
+    return ret
 
 
 def generate_and_save_all_conversations(
-    chats: List[Chat], save_folder_path: str
+    my_name: str,
+    sent_chats: List[Chat],
+    received_chats: List[Chat],
+    save_folder_path: str,
 ) -> None:
     """
     Generates all snapchat chat conversations between all unique sender and receiver pairs and serializes and saves all objects
     to JSON format to the provided save_folder_path. If this folder does not exist, it will be created.
 
-    :param chats: the list of all chat objects
+    :param my_name: your snapchat username
+    :param sent_chats: the list of chats you've sent
+    :param received_chats: the list of chats you've received
     :param save_folder_path: the location to save all the serialized conversations to
     """
-    pass
+
+    if not os.path.exists(save_folder_path):
+        os.makedirs(save_folder_path)
+
+    conversations = generate_conversations(sent_chats, received_chats)
+
+    for conversation in conversations:
+        their_name = next(
+            (
+                their_name
+                for their_name in conversation.get_users()
+                if their_name != my_name
+            ),
+            None,
+        )
+        conversation.to_json(os.path.join(save_folder_path, f"{their_name}.json"))
--- snapsimp/snap_simp.py
+++ snapsimp/snap_simp.py
@@ -1,17 +1,8 @@
 import argparse
-import json
-from snaps.snapchat_snap_conversation import SnapchatSnapConversation
-from chats.snapchat_chat_conversation import SnapchatChatConversation
-from chats.chat_type import ChatType
 from soup.snap_history_parsing import extract_snap_history
 from soup.account_parsing import parse_all
 from soup.chat_history_parsing import extract_chat_history
-from snaps.filtering import (
-    get_by_top_receiver,
-    get_by_top_sender,
-    filter_snaps_by_type,
-)
-from chats.conversation_generator import generate_conversation_with_top_receiver
+from chats.conversation_generator import generate_and_save_all_conversations
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="A parser for Snapchat data exports")
@@ -42,13 +33,15 @@ if __name__ == "__main__":
     received_snaps, sent_snaps = extract_snap_history(
         args.snap_history_file, basic_user_info.username
     )
+    all_snaps = received_snaps + sent_snaps
+
     received_chats, sent_chats = extract_chat_history(
         args.chat_history_file, basic_user_info.username
     )
     all_chats = received_chats + sent_chats
 
-    our_conversation = generate_conversation_with_top_receiver(sent_chats, received_chats)
-    our_conversation.to_json("Her.json")
-    # generate_and_save_all_conversations(all_chats, "all-chat-conversations")
+    our_conversation = generate_and_save_all_conversations(
+        basic_user_info.username, sent_chats, received_chats, "all-chat-conversations"
+    )
 
     print("End Program")
