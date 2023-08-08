f2d69e66352c0b12589f268744c78ea5cc6db497
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2023-08-07 22:04:09 -0500

    frontend vis work

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,10 @@
-import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import {
+  Box,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@mui/material";
 import ourChatsJson from "./darkneonshadows.json";
 
 export default function App() {
@@ -18,7 +24,7 @@ export default function App() {
         main: "#5EBBF9",
       },
       background: {
-        default: "#0f0f0f",
+        default: "#EFEFEF",
       },
     },
     components: {
@@ -58,12 +64,30 @@ export default function App() {
         <Box
           className="custom-scrollbar"
           sx={{
-            background: "#151515",
+            background: "#EFEFEF",
             scroll: "none",
             overflow: "hidden",
           }}
         >
           <CssBaseline />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                backgroundColor: "#5EBBF9",
+                borderRadius: "12px",
+                padding: "5px",
+              }}
+            >
+              <Typography>Lol that one was good</Typography>
+            </Box>
+          </Box>
         </Box>
       </ThemeProvider>
     );
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,13 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import WebFont from "webfontloader";
+
+WebFont.load({
+  google: {
+    families: ["Manrope", "Oswald", "Roboto", "Teko", "Caveat"],
+  },
+});
 
 const rootElement = document.getElementById("root")!;
 const reactRoot = ReactDOM.createRoot(rootElement);
